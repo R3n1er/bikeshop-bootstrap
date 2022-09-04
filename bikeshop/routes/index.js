beff5b5cd62299b2f160f71f86d01b5814f3f0d8
Alban RENIER <renier.alban@gmail.com>
2022-09-04 18:15:18 -0300

    databike add

--- bikeshop/routes/index.js
+++ bikeshop/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+// Création de la liste des vélos
+var dataBike = [
+  { name: "BIK045", url: "/images/bike-1.jpg, price:679" },
+  { name: "ZOO45", url: "/images/bike-2.jpg, price:815" },
+  { name: "TITANS", url: "/images/bike-3.jpg, price:585" },
+  { name: "AMIGO12", url: "/images/bike-4.jpg, price:1025" },
+  { name: "BIK015", url: "/images/bike-5.jpg, price:679" },
+  { name: "ZAMIT15", url: "/images/bike-6.jpg, price:745" },
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bike Shop Experience' });
+});
+
+/* SHOP PAGE. */
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'Bike Shop Experience - SHOP PAGE' });
 });
 
 module.exports = router;
