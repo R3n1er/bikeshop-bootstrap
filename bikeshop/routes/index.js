da5f7fa71dc9b5f0a710d5ecf891369ea1ad8192
Alban RENIER <renier.alban@gmail.com>
2022-09-04 22:55:39 -0300

    modif list of bikes

--- bikeshop/routes/index.js
+++ bikeshop/routes/index.js
@@ -1,25 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // Création de la liste des vélos
 var dataBike = [
-  { name: "BIK045", url: "/images/bike-1.jpg, price:679" },
-  { name: "ZOO45", url: "/images/bike-2.jpg, price:815" },
-  { name: "TITANS", url: "/images/bike-3.jpg, price:585" },
-  { name: "AMIGO12", url: "/images/bike-4.jpg, price:1025" },
-  { name: "BIK015", url: "/images/bike-5.jpg, price:679" },
-  { name: "ZAMIT15", url: "/images/bike-6.jpg, price:745" },
+  { name: "BIK045", url: "/images/bike-1.jpg", price:679 },
+  { name: "ZOO45", url: "/images/bike-2.jpg", price:815 },
+  { name: "TITANS", url: "/images/bike-3.jpg", price:585 },
+  { name: "AMIGO12", url: "/images/bike-4.jpg", price:1025 },
+  { name: "BIK015", url: "/images/bike-5.jpg", price:679 },
+  { name: "ZAMIT15", url: "/images/bike-6.jpg", price:745 },
 ];
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bike Shop Experience' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Bike Shop Experience", dataBike: dataBike });
 });
 
 /* SHOP PAGE. */
-router.get('/shop', function(req, res, next) {
-  res.render('shop', { title: 'Bike Shop Experience - SHOP PAGE' });
+router.get("/shop", function (req, res, next) {
+  res.render("shop", { title: "Bike Shop Experience - SHOP PAGE" });
 });
 
 module.exports = router;
