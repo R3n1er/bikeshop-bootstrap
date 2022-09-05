3e69eccaf20bcb4d92eeac73cf1206fb8a4928f9
Alban RENIER <renier.alban@gmail.com>
2022-09-04 23:07:14 -0300

    modification du panier et preparation du total panier

--- bikeshop/routes/index.js
+++ bikeshop/routes/index.js
@@ -3,13 +3,17 @@ var router = express.Router();
 
 // Création de la liste des vélos
 var dataBike = [
-  { name: "BIK045", url: "/images/bike-1.jpg", price:679 },
-  { name: "ZOO45", url: "/images/bike-2.jpg", price:815 },
-  { name: "TITANS", url: "/images/bike-3.jpg", price:585 },
-  { name: "AMIGO12", url: "/images/bike-4.jpg", price:1025 },
-  { name: "BIK015", url: "/images/bike-5.jpg", price:679 },
-  { name: "ZAMIT15", url: "/images/bike-6.jpg", price:745 },
+  { name: "BIK045", url: "/images/bike-1.jpg", price: 679 },
+  { name: "ZOO45", url: "/images/bike-2.jpg", price: 815 },
+  { name: "TITANS", url: "/images/bike-3.jpg", price: 585 },
+  { name: "AMIGO12", url: "/images/bike-4.jpg", price: 1025 },
+  { name: "BIK015", url: "/images/bike-5.jpg", price: 679 },
+  { name: "ZAMIT15", url: "/images/bike-6.jpg", price: 745 },
 ];
+
+// Variable pour le panier (faut panier)
+
+var dataCardBike = [];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -18,7 +22,10 @@ router.get("/", function (req, res, next) {
 
 /* SHOP PAGE. */
 router.get("/shop", function (req, res, next) {
-  res.render("shop", { title: "Bike Shop Experience - SHOP PAGE" });
+  res.render("shop", {
+    title: "Bike Shop Experience - SHOP PAGE",
+    dataCardBike: dataCardBike,
+  });
 });
 
 module.exports = router;
